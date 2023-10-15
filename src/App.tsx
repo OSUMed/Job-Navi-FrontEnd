45bc0c31ed9ba75f4fcf4e764b6a0252c5b720f4
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-10-15 03:36:20 -0700

    added lazy loading to fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,59 @@
-import React from "react";
-
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Dashboard from "./Components/Dashboard";
-import Contacts from "./Components/Contacts";
-import Applications from "./Components/Applications";
-import Notes from "./Components/Notes";
 import "tailwindcss/tailwind.css";
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { LoginCard } from "./Components/auth/LoginCard";
-import { LogoutCard } from "./Components/auth/LogoutCard";
+
+// Lazily load the Dashboard component
+const Dashboard = React.lazy(() => import("./Components/Dashboard"));
+const Contacts = React.lazy(() => import("./Components/Contacts"));
+const Applications = React.lazy(() => import("./Components/Applications"));
+const Notes = React.lazy(() => import("./Components/Notes"));
+const LogoutCard = React.lazy(() =>
+  import("./Components/auth/LogoutCard").then((module) => ({
+    default: module.LogoutCard,
+  }))
+);
 
 const App: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null); // To store the user object
-  const [loading, setLoading] = useState(true); // To manage a loading state during auth check
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const auth = getAuth();
-    // Listener for auth state (logged in or out)
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser); // If logged in, currentUser is user object, else null
+      setUser(currentUser);
       setLoading(false);
     });
-    // Clean up the listener on component unmount
     return () => unsubscribe();
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>; // Or a loading spinner/component
+    return <div>Loading...</div>;
   }
+
   return (
     <BrowserRouter>
-      <Routes>
-        {user ? (
-          <>
-            <Route path="/" element={<Applications />} />
-            <Route
-              path="contacts"
-              element={<Contacts cookie={{ session: "" }} />}
-            />
-            <Route path="applications" element={<Applications />} />
-            <Route path="notes" element={<Notes />} />
-            <Route path="logout" element={<LogoutCard />} />
-          </>
-        ) : (
-          // Render a login component or redirect to a login page
-          <Route path="/" element={<LoginCard />} />
-        )}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {user ? (
+            <>
+              <Route path="/" element={<Applications />} />
+              <Route
+                path="contacts"
+                element={<Contacts cookie={{ session: "" }} />}
+              />
+              <Route path="applications" element={<Applications />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="notes" element={<Notes />} />
+              <Route path="logout" element={<LogoutCard />} />
+            </>
+          ) : (
+            <Route path="/" element={<LoginCard />} />
+          )}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
